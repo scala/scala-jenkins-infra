14606b88a36a3efa87f3a1f4ed6617138f90ae4c
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-22 14:47:48 -0800

    fixup jenkins version pinning

--- attributes/master.rb
+++ attributes/master.rb
@@ -20,8 +20,9 @@ default['master']['adminAddress'] = "adriaan@typesafe.com"
 default['master']['jenkinsUrl']   = "http://scala-ci.typesafe.com/"
 
 # To pin the jenkins version, must also override override['jenkins']['master']['source'] !!!
-# override['jenkins']['master']['version']        = '1.555'
-# override['jenkins']['master']['checksum']       = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
+# override['jenkins']['master']['version']  = '1.555'
+# override['jenkins']['master']['source']   = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
+# override['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
 
 
 default['master']['env']["ANT_OPTS"]      = "-Xms1536M -Xmx1536M -Xss1M -XX:MaxPermSize=256M -XX:ReservedCodeCacheSize=128M -XX:+UseParallelGC -Dpartest.threads=4"
