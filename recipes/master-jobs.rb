918971127c5de41a2d4f0e5bed26ecb44880717e
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-23 15:18:01 -0800

    move job scripts to repo they act on

--- recipes/master-jobs.rb
+++ recipes/master-jobs.rb
@@ -16,11 +16,30 @@ ruby_block 'set private key' do
   end
 end
 
+windowsBlurb = <<-EOH.gsub(/^ {2}/, '')
+  #!C:/cygwin/bin/bash -ex
+  set -o igncr # ignore crlf issues on cygwin
+
+  source scripts/jobs/$JOB_NAME
+EOH
+
+linuxBlurb = <<-EOH.gsub(/^ {2}/, '')
+  #!/bin/bash -ex
+
+  source scripts/jobs/$JOB_NAME
+EOH
+
+
 %w(scala-release-2.11.x-dist scala-release-2.11.x-scala-lang scala-release-2.11.x-smoketest scala-release-2.11.x-unix scala-release-2.11.x-windows scala-release-2.11.x scala-release-scala-lang-update-current).each do |name|
   xml = File.join(Chef::Config[:file_cache_path], "#{name}.xml")
 
   template xml do
     source "#{name}.xml.erb"
+
+    variables({
+      :jobShellBlurb        => linuxBlurb,
+      :jobShellBlurbWindows => windowsBlurb
+    })
   end
 
   jenkins_job name do
