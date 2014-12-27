2721dee64b6bee3e6d3068ce79bd370bec70ffc7
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-27 00:25:00 -0800

    recursive directory creation doesn't set owner correctly

--- recipes/worker-publish.rb
+++ recipes/worker-publish.rb
@@ -22,12 +22,10 @@ file "#{jenkinsHome}/.ssh/for_chara" do
 end
 
 
-# TODO: verify that all directories are owned by the jenkins user after fresh bootstrap
-# (this is why I have the redundant #{jenkinsHome}/.sbt/0.13)
-["#{jenkinsHome}/.ivy2", "#{jenkinsHome}/.m2", "#{jenkinsHome}/.sbt/0.13", "#{jenkinsHome}/.sbt/0.13/plugins/"].each do |dir|
+# TODO: recursive doesn't set owner correctly (???), so list out all dirs explicitly
+["#{jenkinsHome}/.ivy2", "#{jenkinsHome}/.m2", "#{jenkinsHome}/.sbt", "#{jenkinsHome}/.sbt/0.13", "#{jenkinsHome}/.sbt/0.13/plugins/"].each do |dir|
   directory dir do
     user jenkinsUser
-    recursive true
   end
 end
 
