d97603bfd39368ba1b5648f4c2c2a4426ad45a6f
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-27 00:01:24 -0800

    also make credentials multi-homed

--- recipes/master-workers.rb
+++ recipes/master-workers.rb
@@ -16,24 +16,35 @@ ruby_block 'set private key' do
   end
 end
 
-jenkins_private_key_credentials 'jenkins' do # username == name of resource
-  id '954dd564-ce8c-43d1-bcc5-97abffc81c54'
-  private_key ChefVault::Item.load("master", "scala-jenkins-keypair")['private_key']
+credentialsMap = {
+  'jenkins-pub'  => '954dd564-ce8c-43d1-bcc5-97abffc81c54',
+  'jenkins-priv' => '954dd564-ce8c-43d1-bcc5-97abffc81c55'
+}
+
+privKey = ChefVault::Item.load("master", "scala-jenkins-keypair")['private_key']
+
+# TODO: different keypairs
+credentialsMap.each do |userName, uniqId|
+  jenkins_private_key_credentials userName.dup do # dup is workaround for jenkins cookbook doing a gsub! in convert_to_groovy
+    id uniqId
+    private_key privKey
+  end
 end
+
 
 search(:node, 'name:jenkins-worker* AND os:linux').each do |worker|
   worker["jenkinsHomes"].each do |jenkinsHome, workerConfig|
     jenkins_ssh_slave workerConfig["workerName"] do
       host        worker.ipaddress
-      credentials '954dd564-ce8c-43d1-bcc5-97abffc81c54' # must use id (groovy script fails otherwise)
+      credentials credentialsMap[workerConfig["jenkinsUser"]]  # must use id (groovy script fails otherwise)
+      remote_fs   jenkinsHome.dup
 
       labels      workerConfig["labels"]
-
       executors   workerConfig["executors"]
 
       environment(node["master"]["env"].merge(workerConfig["env"]))
 
-      action [:create, :connect]
+      action [:create, :connect, :online]
     end
   end
 end
