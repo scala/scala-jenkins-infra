eb250d23a5f7de006b44281889e85b15a789d9c0
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-29 12:36:59 -0800

    fixup chef-client

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures the Scala Jenkins infrastructure'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'chef-client'
+depends 'cron'
+
 depends 'aws'
 
 depends 'windows'
