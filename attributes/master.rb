43cfec26db5f157915a1cad76b69f9ef05711e3b
Adriaan Moors <adriaan.moors@typesafe.com>
2014-12-29 11:49:55 -0800

    formatting

--- attributes/master.rb
+++ attributes/master.rb
@@ -1,27 +1,23 @@
-default['master']['github']['webUri'] = 'https://github.com/'
-default['master']['github']['apiUri'] = 'https://api.github.com'
-
-default['master']['github']['adminUserNames']                  = 'adriaanm,chef'
-default['master']['github']['organizationNames']               = 'scala'
-default['master']['github']['useRepositoryPermissions']        = 'true'
-default['master']['github']['allowAnonymousReadPermission']    = 'true'
-default['master']['github']['authenticatedUserReadPermission'] = 'true'
-
-default['master']['github']['allowGithubWebHookPermission'] = 'true'
-default['master']['github']['allowCcTrayPermission']        = 'false'
-
-default['master']['github']['authenticatedUserCreateJobPermission'] = 'false'
-
-
 override['jenkins']['master']['install_method'] = 'war'
-
-default['master']['adminAddress'] = "adriaan@typesafe.com"
-default['master']['jenkinsUrl']   = "http://scala-ci.typesafe.com/"
 
 # To pin the jenkins version, must also override override['jenkins']['master']['source'] !!!
 # override['jenkins']['master']['version']  = '1.555'
 # override['jenkins']['master']['source']   = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
 # override['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
+
+default['master']['github']['webUri']                               = 'https://github.com/'
+default['master']['github']['apiUri']                               = 'https://api.github.com'
+default['master']['github']['adminUserNames']                       = 'adriaanm,chef'
+default['master']['github']['organizationNames']                    = 'scala'
+default['master']['github']['useRepositoryPermissions']             = 'true'
+default['master']['github']['allowAnonymousReadPermission']         = 'true'
+default['master']['github']['authenticatedUserReadPermission']      = 'true'
+default['master']['github']['allowGithubWebHookPermission']         = 'true'
+default['master']['github']['allowCcTrayPermission']                = 'false'
+default['master']['github']['authenticatedUserCreateJobPermission'] = 'false'
+
+default['master']['adminAddress'] = "adriaan@typesafe.com"
+default['master']['jenkinsUrl']   = "http://scala-ci.typesafe.com/"
 
 
 default['master']['env']["ANT_OPTS"]      = "-Xms1536M -Xmx1536M -Xss1M -XX:MaxPermSize=256M -XX:ReservedCodeCacheSize=128M -XX:+UseParallelGC -Dpartest.threads=4"
--- attributes/windows.rb
+++ attributes/windows.rb
@@ -3,17 +3,15 @@ if platform_family?("windows")
   override['java']['windows']['package_name'] = 'Java(TM) SE Development Kit 6 (64-bit)'
   override['java']['windows']['url']          = 'https://dl.dropboxusercontent.com/u/12862572/jdk-6u45-windows-x64.exe'
   override['java']['windows']['checksum']     = '345059d5bc64275c1d8fdc03625d69c16d0c8730be1c152247f5f96d00b21b00'
-  override['java']['javacVersion']            = "javac 1.6.0_45" # we don't install if javac -version returns this string
-
-  # must specify java_home on windows (issues with installer on reinstall if it's in program files)
-  override['java']['java_home'] = 'C:\java\jdk-1.6'
+  override['java']['javacVersion']            = "javac 1.6.0_45"  # we don't install if javac -version returns this string
+  override['java']['java_home']               = 'C:\java\jdk-1.6' # must specify java_home on windows (issues with installer on reinstall if it's in program files)
 
   override['sbt']['script_name']   = 'sbt.bat'
   override['sbt']['launcher_path'] = 'C:\sbt'
   override['sbt']['bin_path']      = 'C:\sbt'
 
+  # this zip was reworked to have the binaries under the `bin/` directory, which is what sbt-nativepackager expects
   override['wix']['home']     = 'C:\WIX'
-  # this zip was rework to have the binaries under the `bin/` directory, which is what sbt-nativepackage expects
   override['wix']['url']      = 'https://dl.dropboxusercontent.com/u/12862572/wix39-binaries.zip'
   override['wix']['checksum'] = '1f509e61462e49918bf7932e42a78bbc60e6125b712995f55279a6d721f00602'
 
