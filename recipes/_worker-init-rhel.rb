f7241cc849d01986d5c5f78a98616aff30462e08
Adriaan Moors <adriaan.moors@typesafe.com>
2015-01-09 20:51:09 -0800

    create /usr/lib/jvm-exports/java to use oracle jdk
    Running ant failed with '/usr/bin/build-classpath: error:
    JVM_LIBDIR /usr/lib/jvm-exports/java does not exist or is not a directory'
    
    Apparently, on centos/amazon linux, java packages assume the following:
    JAVA_HOME=/usr/lib/jvm/java implies there must be a /usr/lib/jvm*-exports*/java
    
    see http://www.karakas-online.de/forum/viewtopic.php?t=9781

--- recipes/_worker-init-rhel.rb
+++ recipes/_worker-init-rhel.rb
@@ -9,6 +9,11 @@
 
 include_recipe "java"
 
+# /usr/bin/build-classpath: error: JVM_LIBDIR /usr/lib/jvm-exports/java does not exist or is not a directory
+# http://www.karakas-online.de/forum/viewtopic.php?t=9781
+# JAVA_HOME=/usr/lib/jvm/java implies there must be a /usr/lib/jvm-exports/java, which is not created by the oracle rpm
+directory '/usr/lib/jvm-exports/java'
+
 %w{ant}.each do |pkg|
   package pkg
 end
