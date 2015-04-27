6f63c1205ba1283ec48b36eb738e4b7e8ff78f46
Adriaan Moors <adriaan.moors@typesafe.com>
2015-04-27 15:06:54 -0700

    vagrantfile
    vagrant uses bridged networking -- use local /etc/hosts
    to point scala-ci.typesafe.com at the VM's ip test jenkins locally.
    
    (portforwarding wreaks havoc with reverse proxy & ssl)

--- recipes/master-init.rb
+++ recipes/master-init.rb
@@ -13,8 +13,11 @@ chef_gem "chef-vault"
 include_recipe "scala-jenkins-infra::_master-init-jenkins"
 
 
-# https://github.com/scala/scala-jenkins-infra/issues/26
-# workaround from https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1317811/comments/22 (until we can upgrade to kernel with fix -- >3.16.1)
-execute 'turn off scatter-gatter' do
-  command "ethtool -K eth0 sg off"
+case node["platform"]
+when "amazon"
+  # https://github.com/scala/scala-jenkins-infra/issues/26
+  # workaround from https://bugs.launchpad.net/ubuntu/+source/linux/+bug/1317811/comments/22 (until we can upgrade to kernel with fix -- >3.16.1)
+  execute 'turn off scatter-gatter' do
+    command "ethtool -K eth0 sg off"
+  end
 end
